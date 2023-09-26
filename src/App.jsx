72bd6a0626c9be7b41fb69b2a995df171bf9dc6d
Oliwia <itsechi@gmail.com>
2023-09-26 13:47:48 +0200

    Add projects component and properly fix the blob container z-index

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
 import { Nav } from "./components/Nav/Nav";
 import { Cursor } from "./components/Cursor/Cursor";
 import { Home } from "./components/Home/Home";
+import { Projects } from "./components/Projects/Projects";
 
 export const App = () => {
   return (
     <>
       <Cursor />
       <Nav />
-      <Home />
+      {/* <Home /> */}
+      <Projects />
     </>
   );
 };
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,21 @@
+import styles from "./Projects.module.scss";
+
+export const Projects = () => {
+  return (
+    <main>
+      <div>
+        <div>
+          <p>React</p>
+        </div>
+        <h2>Weather app</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, velit
+          aliquam autem omnis soluta explicabo earum! Maxime natus similique
+          enim blanditiis doloremque illum incidunt? Ex repellat perspiciatis
+          cumque molestiae fuga!
+        </p>
+        <a href="#">View website</a>
+      </div>
+    </main>
+  );
+};
